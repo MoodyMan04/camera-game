3b1ee86c0b381d9ab7f320a813c6ccd27930a4b0
Gunnar Moody <115500822+MoodyMan04@users.noreply.github.com>
2023-07-11 23:34:36 -0500

    animation and mechanics
    added the following:
    - sprinting (basics)
    - crouching (basics)
    - camera tilt when going left or right
    - camera bob for:
      > idle
      > walking
      > sprinting

--- Camera Game/Assets/Scripts/PlayerCameraController.cs
+++ Camera Game/Assets/Scripts/PlayerCameraController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Moody 20230710
+// Moody 20230711
 // Code taken / inspired by Brackeys
 /**
  * Class to implement the ability to move the main camera as a first person player
@@ -11,15 +11,22 @@ using UnityEngine;
 public class PlayerCameraController : MonoBehaviour
 {
     
-    public float mouseSen = 100f;
+    public float mouseSen = 1000f;
+    public float rotAmount = 2f;
 
+    public Animator camAnim;
+    
     public Transform playerBody;
 
     private float _xRotation = 0f;
-    
+    private static readonly int Walk = Animator.StringToHash("walk");
+    private static readonly int Idle = Animator.StringToHash("idle");
+    private static readonly int Sprint = Animator.StringToHash("sprint");
+
     // Start is called before the first frame update
     void Start()
     {
+        
         // Lock cursor to center of screen
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -27,6 +34,9 @@ public class PlayerCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CameraRot();
+        CameraBob();
+        
         // Time.deltaTime gets time between updates, this here helps prevents faster sensitivity due to higher frame rates
         float mouseX = Input.GetAxis("Mouse X") * mouseSen * Time.deltaTime; 
         float mouseY = Input.GetAxis("Mouse Y") * mouseSen * Time.deltaTime;
@@ -36,8 +46,47 @@ public class PlayerCameraController : MonoBehaviour
         _xRotation = Math.Clamp(_xRotation, -90f, 90f);
         
         // Move camera accordingly
-        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        Vector3 v = transform.rotation.eulerAngles;
+        transform.localRotation = Quaternion.Euler(_xRotation, 0, v.z);
         playerBody.Rotate(Vector3.up * mouseX);
         
+    }
+
+    void CameraRot()
+    {
+        // Tilt camera left or right upon movement
+        float rotZ = -Input.GetAxis("Horizontal") * rotAmount;
+        
+        Quaternion finalRot = Quaternion.Euler(0, 0, rotZ);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, finalRot, 1f);
+        
+    }
+
+    void CameraBob()
+    {
+        // Bob camera up and down when moving depending on type of movement (using animation)
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) &&
+                (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl)))
+            {
+                camAnim.ResetTrigger(Idle);
+                camAnim.SetTrigger(Sprint);
+            }
+            else
+            {
+                camAnim.ResetTrigger(Idle);
+                camAnim.ResetTrigger(Walk);
+                camAnim.SetTrigger(Walk);
+
+            }
+        }
+        else
+        {
+            camAnim.ResetTrigger(Walk);
+            camAnim.ResetTrigger(Sprint);
+            camAnim.SetTrigger(Idle);
+        }
+
     }
 }
